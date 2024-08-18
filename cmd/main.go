4c891e1274d566a81246f21f5fdcc706bbb74a2e
黄蒙蒙 <hmm@haier.net>
2024-08-18 10:04:25 +0800

    调整回调路径

--- cmd/main.go
+++ cmd/main.go
@@ -7,17 +7,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/volcengine/volc-sdk-golang/example/rtc/v20230801"
-	"github.com/volcengine/volc-sdk-golang/example/rtc/v20231101"
+	// "github.com/volcengine/volc-sdk-golang/example/rtc/v20230801"
+	// "github.com/volcengine/volc-sdk-golang/example/rtc/v20231101"
 )
 
 // 不同类型的单元测试
 // https://stackoverflow.com/questions/19998250/proper-package-naming-for-testing-with-the-go-language
 func main() {
-	rtc_v20230801.GetRoomOnlineUsers()
-	rtc_v20231101.ListDetectionTasks()
-	// server()
+	// rtc_v20230801.GetRoomOnlineUsers()
+	// rtc_v20231101.ListDetectionTasks()
+	server()
 }
 
 func server() {
@@ -25,7 +24,7 @@ func server() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong\n")
 	})
-	r.POST("/rtc/room", Callback)
+	r.POST("/rtc", Callback)
 	r.Run(":8080")
 }
 
